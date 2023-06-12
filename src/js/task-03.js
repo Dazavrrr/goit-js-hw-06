186da9e4e40d27d8aab0972af87f3604d8832b83
Dazavrrr <damelasha@gmail.com>
2023-06-12 11:40:26 +0300

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector(".gallery");
+const galleryItems = images.map((image) => {
+  return `<li><img src="${image.url}" alt="${image.alt}" width="300"></li>`;
+});
+
+gallery.insertAdjacentHTML("beforeend", galleryItems.join(""));
