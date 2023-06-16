85ce888a66d4e55c8f307391f06afcb743ef03e0
Dazavrrr <damelasha@gmail.com>
2023-06-16 13:02:20 +0300

    Update task-05.js

---
+++ src/js/task-05.js
@@ -0,0 +1,14 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', updateName);
+
+function updateName() {
+  const name = nameInput.value;
+
+  if (name === "") {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = name;
+  }
+}
